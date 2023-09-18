41bdca04541fa83af7e260fa13385ddcaadac226
VadymPoliak <witekompani@gmail.com>
2023-09-18 04:45:59 -0400

    ACTIVEDEV-20 fix leverage for public and private data and multi currency

--- src/handlers/leverages.py
+++ src/handlers/leverages.py
@@ -42,48 +42,69 @@ class Leverages:
         
         try:
             query = {}
-            if client:
-                query["client"] = client
             if exchange:
                 query["venue"] = exchange
-            if account:
-                query["account"] = account
             
             # fetch latest position, balance, tickers
+            ticker_value = self.tickers_db.find(query).sort('runid', -1).limit(1)
+            for item in ticker_value:
+                try: 
+                    latest_ticker = item['ticker_value']
+                except Exception as e:
+                    print("An error occurred in Leverages:", e)
+                    return False  
+                
+            if client:
+                query["client"] = client
+            if account:
+                query["account"] = account
+
             position_value = self.positions_db.find(query).sort('runid', -1).limit(1)
             for item in position_value:
                 try:
                     latest_position = item['position_value']
-                except:
+                except Exception as e:
+                    print("An error occurred in Leverages:", e)
                     return False
             
             balance_valule = self.balances_db.find(query).sort('runid', -1).limit(1)
             for item in balance_valule:
                 try: 
                     latest_balance = item['balance_value']
-                except:
+                except Exception as e:
+                    print("An error occurred in Leverages:", e)
                     return False
-            
-            ticker_value = self.tickers_db.find(query).sort('runid', -1).limit(1)
-            for item in ticker_value:
-                try: 
-                    latest_ticker = item['ticker_value']['BTC/USDT']['last']
-                except:
-                    return False               
 
             try:
                 max_notional = abs(float(max(latest_position, key=lambda x: abs(float(x['notional'])))['notional']))
-            except:
+            except Exception as e:
+                print("An error occurred in Leverages:", e)
                 return False
             
             base_currency = config['balances']['base_ccy']
 
             balance_in_base_currency = 0
-            for currency, balance in latest_balance.items():
-                if currency == base_currency:
-                    balance_in_base_currency += balance
+            try:
+                if base_currency == "USDT":
+                    balance_in_base_currency = latest_balance['USD'] / latest_ticker['USDT/USD']['last']
+                elif base_currency == "USD":
+                    balance_in_base_currency = latest_balance['USD'] 
                 else:
-                    balance_in_base_currency += latest_ticker * balance
+                    balance_in_base_currency = latest_balance['USD'] / (latest_ticker['USDT/USD']['last'] * latest_ticker[base_currency + '/USDT']['last'])
+            except Exception as e:
+                print("An error occurred in Leverages:", e)
+                return False
+
+            # try:
+            #     balance_in_base_currency = 0
+            #     for currency, balance in latest_balance.items():
+            #         if currency == base_currency:
+            #             balance_in_base_currency += balance
+            #         else:
+            #             balance_in_base_currency += latest_ticker * balance
+            # except Exception as e:
+            #     print("An error occurred in Leverages:", e)
+            #     return False
 
             leverage_value = {
                 "client": client,
@@ -93,8 +114,10 @@ class Leverages:
             }
             try:
                 leverage_value['leverage'] = max_notional / balance_in_base_currency
-            except:
+            except Exception as e:
+                print("An error occurred in Leverages:", e)
                 return False
+            
             leverage_value["runid"] = latest_run_id
             
             if config['leverages']['store_type'] == "timeseries":
