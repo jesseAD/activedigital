8e63e5eb1062b35b13703888484fafe87798cf30
douggie <douggie@users.noreply.github.com>
2023-11-20 21:33:44 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,5 +4,5 @@ cd "/data/datacollector"
 export MALLOC_ARENA_MAX=4
 cat /dev/null > nohup.out
 source /home/ec2-user/datacollector/bin/activate
-/usr/bin/python3.11 -m pip install --upgrade --force-reinstall -r /data/datacollector/requirements.txt
+pip install --upgrade --force-reinstall -r /data/datacollector/requirements.txt
 exit
