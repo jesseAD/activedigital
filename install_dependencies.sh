d3154a32a295abeeb6b529d20eaaf2e805cf4c0c
m1dnightc0der <97907762+m1dnightc0der@users.noreply.github.com>
2023-11-28 20:03:25 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 source /home/ec2-user/.bash_profile
 cd "/data/datacollector"
+cp -R /data/datacollector/.config/dask/ /home/ec2-user/.config/dask
 export MALLOC_ARENA_MAX=4
 cat /dev/null > nohup.out
 source /home/ec2-user/datacollector/bin/activate
