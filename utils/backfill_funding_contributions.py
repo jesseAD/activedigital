8435155df6d33a3478d77bbd1f12b928aa9bc7fd
angeltech8221 <angel.tech8221@gmail.com>
2024-08-01 03:21:55 -0400

    ACTIVEDEV: rename vip_level to tier

--- utils/backfill_funding_contributions.py
+++ utils/backfill_funding_contributions.py
@@ -16,12 +16,27 @@ load_dotenv()
 
 mongo_uri = 'mongodb+srv://activedigital:'+os.getenv("CLOUD_MONGO_PASSWORD")+'@mongodbcluster.nzphth1.mongodb.net/?retryWrites=true&w=majority'
 db = pymongo.MongoClient(mongo_uri)
+collection = db['active_digital']['balances']
 
 for client in config['clients']:
   for exchange in config['clients'][client]['subaccounts']:
     for account in config['clients'][client]['subaccounts'][exchange]:
-      FundingContributions(db, "temp_collection").create(
-        client=client,
-        exchange=exchange,
-        account=account
-      )
+      balance = list(collection.find({
+        'client': client,
+        'venue': exchange,
+        'account': account,
+        'runid': 70651
+      }))
+
+      if len(balance) > 0:
+        vip_level = balance[0]['vip_level']
+
+        collection.update_many(
+          {'runid': {'$lt': 70651}, 'client': client, 'venue': exchange, 'account': account},
+          {'$set': {'vip_level': vip_level}}
+        )
+      # FundingContributions(db, "temp_collection").create(
+      #   client=client,
+      #   exchange=exchange,
+      #   account=account
+      # )
